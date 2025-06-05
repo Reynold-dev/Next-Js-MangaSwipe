501c7586ca35af674b151f670a0b10136154c306
Reynold-dev <reynoldarun89@gmail.com>
2025-06-05 16:35:08 +0530

    feat: added theme/meta-data

---
+++ src/lib/config/siteConfig.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  title: "MangaSwipe",
+  description: "Manga Swipe is the best manga/manhwa website in the world.",
+  keywords: ["MangaSwipe", "Manga", "Manhwa", "Anime"],
+};
+
+export type SiteConfig = typeof siteConfig;
